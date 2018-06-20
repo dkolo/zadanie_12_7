672e89deff742f0f256597d58e848c6862d00bb8
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-06-20 16:33:06 +0200

    Add setupCards function in js/App.js file

--- js/App.js
+++ js/App.js
@@ -2,29 +2,37 @@
 
 var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
 var myHeaders = {
-  'X-Client-Id': 'X-Client-Id',
-  'X-Auth-Token': 'X-Auth-Token'
+    'X-Client-Id': 'X-Client-Id',
+    'X-Auth-Token': 'X-Auth-Token'
 };
 
 $.ajaxSetup({
-	headers: myHeaders
+    headers: myHeaders
 });
 
 $.ajax({
     url: baseUrl + '/board',
     method: 'GET',
     success: function(response) {
-      setupColumns(response.columns);
+        setupColumns(response.columns);
     }
 });
 
 // TWORZENIE KOLUMN
 
 function setupColumns(columns) {
-    columns.forEach(function (column) {
-  		var col = new Column(column.id, column.name);
+    columns.forEach(function(column) {
+        var col = new Column(column.id, column.name);
         board.createColumn(col);
+        setupCards(col, column.cards);
     });
+}
+
+function setupCards(col, cards) {
+    cards.forEach(function(card) {
+        var cardObj = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+        col.createCard(cardObj);
+    })
 }
 
 // TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
