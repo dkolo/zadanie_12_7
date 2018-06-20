4e44b305d40c7d4cc6ac8834fec2a5094c4b1998
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-06-20 18:36:29 +0200

    Fix bug in js/App.js

--- js/App.js
+++ js/App.js
@@ -2,8 +2,8 @@
 
 var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
 var myHeaders = {
-    'X-Client-Id': 'X-Client-Id',
-    'X-Auth-Token': 'X-Auth-Token'
+    'X-Client-Id': '3122',
+    'X-Auth-Token': '507764fa12e794c4df0e8e1a878fe0a5'
 };
 
 $.ajaxSetup({
@@ -24,13 +24,12 @@ function setupColumns(columns) {
     columns.forEach(function(column) {
         var col = new Column(column.id, column.name);
         board.createColumn(col);
-        setupCards(col, column.cards);
     });
 }
 
 function setupCards(col, cards) {
     cards.forEach(function(card) {
-        var cardObj = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
-        col.createCard(cardObj);
+        var $card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+        col.addCard(card);
     });
 }
