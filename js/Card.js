1d6f929e1a8958739528655adcaecd8a88b72f2c
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-06-21 10:15:35 +0200

    Add commentend editCard function

--- js/Card.js
+++ js/Card.js
@@ -9,26 +9,50 @@ function Card(id, name) {
     function createCard() {
         var card = $('<li class="card"></li>');
         var cardDeleteBtn = $('<button class="btn-delete">x</button>');
+        var cardEditBtn = $('<button class="btn-edit">Edit</button>')
         var cardDescription = $('<p class="card-description"></p>');
 
         cardDeleteBtn.click(function() {
             self.removeCard();
         });
+        // Event handler for editCard function
+        /*cardEditBtn.click(function() {
+                self.editCard(card);
+            }*/
 
-        card.append(cardDeleteBtn);
-        cardDescription.text(self.name);
-        card.append(cardDescription);
-        return card;
+            // End of event handler for editCard function
+            card.append(cardDeleteBtn); cardDescription.text(self.name); card.append(cardDescription);
+            return card;
+        }
     }
-}
-Card.prototype = {
+    Card.prototype = {
+        //Start editCard function
+        /*editCard: function(card) {
+            var newCardName = prompt('Enter new task description');
+            this.name = newCardName;
+            event.preventDefault();
+            $.ajax({
+                    url: baseUrl + '/card/' + self.id,
+                    method: 'PUT',
+                    data: {
+                        name: newCardName,
+                        bootcamp_kanban_column_id: self.id
+                    },
+                    success: function(response) {
+                        var card = new Card(response.id, newCardName);
+                    });
+            });*/
+        /*Przeniesione z Column.js
+        this.element.text(card.element);
+    }*/
+    //End of editCard function
     removeCard: function() {
         var self = this;
         $.ajax({
             url: baseUrl + '/card/' + self.id,
             method: 'DELETE',
             success: function() {
-                self.$element.remove();
+                self.element.remove();
             }
         });
     }
